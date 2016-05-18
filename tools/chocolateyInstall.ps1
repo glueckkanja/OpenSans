3ccb6154c17251fbcf274ca2faccfec022f57cfb
Jan Berdel <jan.berdel@glueckkanja.com>
2016-05-18 11:38:00 +0200

    Initial commit
    Package is based on the sourcecodepro package by digitaldrummerj:
    https://chocolatey.org/packages/SourceCodePro

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,41 @@
+function Get-CurrentDirectory
+{
+  $thisName = $MyInvocation.MyCommand.Name
+  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+}
+
+$package = 'OpenSans'
+
+$fontHelpersPath = (Join-Path (Get-CurrentDirectory) 'FontHelpers.ps1')
+. $fontHelpersPath
+
+# collection url:
+# https://www.google.com/fonts#UsePlace:use/Collection:Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic|Open+Sans+Condensed:300,300italic,700
+$fontUrl = 'https://www.google.com/fonts/download?kit=3hvsV99qyKCBS55e5pvb3k_UpNCUsIj1Q-eLvtScfRfjeAZG0syLSF0MtprGrcoF' # fonts.zip
+$destination = Join-Path $Env:Temp 'OpenSans'
+
+Install-ChocolateyZipPackage -url $fontUrl -unzipLocation $destination
+
+$shell = New-Object -ComObject Shell.Application
+$fontsFolder = $shell.Namespace(0x14)
+
+$fontFiles = Get-ChildItem $destination -Recurse -Filter *.ttf
+
+# unfortunately the font install process totally ignores shell flags :(
+# http://social.technet.microsoft.com/Forums/en-IE/winserverpowershell/thread/fcc98ba5-6ce4-466b-a927-bb2cc3851b59
+# so resort to a nasty hack of compiling some C#, and running as admin instead of just using CopyHere(file, options)
+$commands = $fontFiles |
+% { Join-Path $fontsFolder.Self.Path $_.Name } |
+? { Test-Path $_ } |
+% { "Remove-SingleFont '$_' -Force;" }
+
+# http://blogs.technet.com/b/deploymentguys/archive/2010/12/04/adding-and-removing-fonts-with-windows-powershell.aspx
+$fontFiles |
+% { $commands += "Add-SingleFont '$($_.FullName)';" }
+
+$toExecute = ". $fontHelpersPath;" + ($commands -join ';')
+Start-ChocolateyProcessAsAdmin $toExecute
+
+Remove-Item $destination -Recurse
+
+
